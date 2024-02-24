88f3f2369d983a1995ee38b16d9b251c25cdc423
Wellington Santos <wellington.santos.tam@vector-unitech.com>
2024-02-24 12:14:55 -0300

    feat: test action

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // Importando o arquivo de configuração do Storybook
+      '@storybook/react': path.resolve(__dirname, '.storybook'),
+    },
+  },
+});
